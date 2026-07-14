dd8b504b4a692d3353c5b7779d29091108b24766
guoqiang <guoqiang.zhu@ubixai.com>
2026-07-14 15:05:48 +0800

    update UBiddingGdtAdapter 4.15.90.2

---
+++ UBiddingGdtAdapter.podspec
@@ -0,0 +1,16 @@
+
+Pod::Spec.new do |s|
+  s.name             = 'UBiddingGdtAdapter'
+  s.version          = '4.15.90.2'
+  s.summary          = 'UBiddingGdtAdapter'
+  s.homepage         = 'https://www.ubixai.com/product/md'
+  s.author           = { 'zhugq' => 'guoqiang.zhu@ubixai.com' }
+  s.source           = { :git => 'https://github.com/ubixai/UBiddingGdtAdapter.git', :tag => s.version.to_s }
+  s.ios.deployment_target = '12.0'
+
+  s.static_framework = true
+  s.vendored_frameworks = 'UBiddingGdtAdapter.xcframework'
+
+  s.dependency 'GDTMobSDK', '4.15.90'
+  s.dependency 'UBiddingAdSDK'
+end
---
+++ UBiddingGdtAdapter.xcframework/ios-arm64/UBiddingGdtAdapter.framework/Headers/UBiddingGdtAdapter.h
@@ -0,0 +1,23 @@
+//
+//  UBiddingGdtAdapter.h
+//  UBiddingGdtAdapter
+//
+//  Created by guoqiang.zhu on 2024/6/19.
+//  Copyright © 2024 UBiX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//@interface UBiddingGdtAdapter : NSObject
+//
+//@end
+
+//! Project version number for UBiddingGdtAdapter.
+FOUNDATION_EXPORT double UBiddingGdtAdapterVersionNumber;
+
+//! Project version string for UBiddingGdtAdapter.
+FOUNDATION_EXPORT const unsigned char UBiddingGdtAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like
+
+
